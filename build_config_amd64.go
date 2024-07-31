21c8d2677f7fcba65b32fe451a0f1b4d5486c2ba
jdelrue <jonas.delrue@jimber.io>
2024-07-31 19:54:22 +0200

    remove build stuff for mips, do in env

--- build_config_amd64.go
+++ build_config_amd64.go
@@ -1,5 +1,7 @@
-// +build linux,amd64
+//go:build linux && amd64
+
 package netfilter
+
 /*
 #cgo CFLAGS: -Wall -I/usr/include
 #cgo LDFLAGS: -L/usr/lib/mips-linux-gnu -lnetfilter_queue
--- build_config_mips.go
+++ build_config_mips.go
@@ -1,8 +1,5 @@
-// +build linux,mips
+//go:build linux && mips
+
 package netfilter
-/*
-#cgo pkg-config: libnetfilter_queue
-#cgo CFLAGS: -Wall -I/usr/include
-#cgo LDFLAGS: -L/usr/lib/mips-linux-gnu
-*/
+
 import "C"
